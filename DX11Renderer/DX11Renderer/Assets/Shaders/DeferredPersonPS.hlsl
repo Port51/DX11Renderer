8b45cf5d043f2b43740c7f3d5e67d342ac0103d1
Bryan Franz <bfranz@mail.usf.edu>
2022-08-06 17:37:03 -0400

    Fresnel

--- DX11Renderer/DX11Renderer/Assets/Shaders/DeferredPersonPS.hlsl
+++ DX11Renderer/DX11Renderer/Assets/Shaders/DeferredPersonPS.hlsl
@@ -37,6 +37,10 @@ float4 main(v2f i) : SV_Target
 
     float3 combinedLight = specularLight.rgb + diffuseLight.rgb;
 
+    // Fresnel
+    float3 viewDirVS = normalize(i.positionVS);
+    float fresnel = pow(saturate(1.0 - dot(i.normalVS, -viewDirVS)), 1.5f);
+
     // Color selection
     const float3 skinColor = float3(255.f / 256.f, 174.f / 256.f, 0.f / 256.f);
     const float3 clothColor = float3(0.f / 256.f, 51.f / 256.f, 256.f / 256.f);
@@ -45,7 +49,7 @@ float4 main(v2f i) : SV_Target
     float3 color = lerp(magicColor, lerp(skinColor, lerp(staffColor, clothColor, i.vertColor.g), i.vertColor.r), i.vertColor.b);
 
     // Glowy gem on end of staff
-    float3 emission = float3(32.f / 256.f, 255.f / 256.f, 47.f / 256.f) * (1.0 - i.vertColor.b) * 2.0f;
+    float3 emission = float3(32.f / 256.f, 255.f / 256.f, 47.f / 256.f) * (1.0 - i.vertColor.b) * (0.2f + 2.8f * fresnel);
 
     return float4(combinedLight.rgb * color + emission, 1);
 }
